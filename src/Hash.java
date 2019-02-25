c0e92f1fd9baf7579d00e063742516211e4e8881
lorchrob <lorchrob@gmail.com>
2019-02-24 21:01:39 -0600

    Write class Hash except method toString

---
+++ src/Hash.java
@@ -0,0 +1,39 @@
+import java.nio.ByteBuffer;
+import java.security.MessageDigest;
+
+public class Hash {
+  byte[] data;
+
+  public Hash(byte[] data) {
+    this.data = data;
+  }
+
+  public byte[] getData() {
+    return this.data;
+  }
+
+  public boolean isValid() {
+    if (this.data[0] == 0 && this.data[1] == 0 && this.data[2] == 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public String toString() {
+    String hex = "";
+    for (int i = 0; i < data.length; i++) {
+      hex += String.format(Byte.toUnsignedInt(data[i]));
+    }
+    return hex;
+  }
+
+  public boolean equals(Object other) {
+    if (other instanceof Hash) {
+      Hash o = (Hash) other;
+      return o.data.equals(this.data);
+    } else {
+      return false;
+    }
+  }
+}
